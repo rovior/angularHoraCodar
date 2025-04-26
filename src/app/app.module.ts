074fd9754e5b10fc8396b430ce2cc273ed765263
rovior <robertbarbosabarros@gmail.com>
2025-04-26 20:24:16 -0300

    feat: aprendendo a usar rederizacao condicional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 //importando componente
 import {FirstComponentComponent} from './components/first-component/first-component.component';
 import { ParentDataComponent } from './components/parent-data/parent-data.component';
-import { DirectivesComponent } from './components/directives/directives.component'
+import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component'
 
 @NgModule({
   declarations: [
     AppComponent, //declrando compoente para que sea reconhecido
     FirstComponentComponent,
     ParentDataComponent,
-    DirectivesComponent //declranado compoenente para que seja reconhecido 
+    DirectivesComponent,
+    IfRenderComponent //declranado compoenente para que seja reconhecido 
   ],
   imports: [BrowserModule],
   providers: [],
